36f28dfd22f0dcc86420f51b58b9b5c929ec7f6a
skyprimini <syannevoxland@gmail.com>
2025-02-26 11:49:25 -0600

    #New Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { Course } from './model/course';
+import { COURSES } from './data/course-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'practice-Component';
+  CSci160: Course = COURSES[0];
+  CSci161: Course = COURSES[1];
+  CSci213: Course = COURSES[2];
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input() course!: Course;
+}
